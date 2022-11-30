0eb3cf5c7f363b0f432e43addf312d61730921c9
Anirban Chakraborty <anchakrabort@glanbia.ie>
2022-11-30 12:10:04 +0000

    feature/3

--- backend/server.php
+++ backend/server.php
@@ -4,32 +4,34 @@
     header('Access-Control-Allow-Methods: POST');
     
     header("Access-Control-Allow-Headers: X-Requested-With");
-    
-    if($_SERVER['REQUEST_METHOD']=="POST"){
-        convert();
-    }
+
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+         convert();
+        }else{
+            http_response_code(404);
+            echo 'Method not allowed';
+        }
 
     function convert(){
 
         $data = json_decode(file_get_contents('php://input'));
-        $ch = curl_init();
+        $curl = curl_init();
 
         $content = $data->content;
         $field = "csv=$content";
         $url="http://localhost:3009/convert";
         $header="Content-Type: application/x-www-form-urlencoded";
 
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$field);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_URL,$url);
+        curl_setopt($curl, CURLOPT_POSTFIELDS,$field);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array($header));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $server_output = curl_exec ($ch);
+        $server_output = curl_exec ($curl);
         
-        curl_close($ch);
+        curl_close($curl);
         
         echo $server_output;
     }
-
     
 ?>
